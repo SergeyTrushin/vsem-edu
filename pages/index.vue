d3711bc4fc4a28fd84c3aba75eab7ab1b7543b37
Sergey <sergeytrushin-17@yandex.ru>
2021-02-12 11:21:27 +0400

    start nuxt project

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+<p class="main-title">hello</p>
+<p class="title">
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet perferendis harum nam maxime cum, explicabo doloribus in non neque natus dolore sed quae id, facilis et repellat optio sit alias!
+</p>
+<p class="text">Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Quisquam temporibus, commodi iure, voluptas earum quasi eius quibusdam eum voluptatem error corporis fugiat dicta aspernatur. Magni ad ullam ea, eum deserunt.</p>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+</style>
